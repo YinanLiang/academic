71ac84c1bc438b8e43510d6f315a4b9b069af892
Yinan Liang <yinanliang.au@gmail.com>
2015-07-19 10:54:22 +0800

    Change the header and footer to Material Desgin

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,29 +6,14 @@
  * Time: 7:30 PM
  */
 ?>
-
-</div> <!-- Close of Container div in header -->
-
-<footer class="footer">
-    <div class="container">
-
-        <div class="row">
-
-            <div class="col-md-6">
-                <p class="text-muted">Client Name.</p>
-            </div>
-
-            <div class="col-md-6">
-                <p class="text-muted text-right">Developed By: XYZ</p>
-            </div>
-
         </div>
+        <!-- / .mdl-grid demo-content -->
 
-    </div>
-</footer>
+    </main>
+    <!-- / .mdl-layout__content... -->
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+</div>
+<!-- /.demo-layout... -->
 
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,53 +12,70 @@ echo doctype('html5');
 <html>
 <head>
     <title>Academic Section</title>
-    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css");?>" />
-    <link rel="stylesheet" href="<?php echo base_url("assets/css/sticky-footer-navbar.css");?>" />
+    <link rel="stylesheet" href="<?php echo base_url("assets/material.min.css");?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/styles.css");?>">
+    <script src="<?php echo base_url("assets/material.min.js");?>"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 
 <body>
 
-<!-- Static navbar -->
-<nav class="navbar navbar-default navbar-static-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
+<div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+    <!-- header -->
+    <header class="demo-header mdl-layout__header mdl-color--white mdl-color--grey-100 mdl-color-text--grey-600">
+        <div class="mdl-layout__header-row">
+            <span class="mdl-layout-title">Academic Section</span>
+            <div class="mdl-layout-spacer"></div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+                <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
+                    <i class="material-icons">search</i>
+                </label>
+                <div class="mdl-textfield__expandable-holder">
+                    <input class="mdl-textfield__input" type="text" id="search" />
+                    <label class="mdl-textfield__label" for="search">Enter your query...</label>
+                </div>
+            </div>
+            <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
+                <i class="material-icons">more_vert</i>
             </button>
-            <a class="navbar-brand" href="<?php echo base_url()?>">Academic Section</a>
+            <ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" for="hdrbtn">
+                <li class="mdl-menu__item">My Profile</li>
+                <li class="mdl-menu__item">Feedback</li>
+                <li class="mdl-menu__item">Logout</li>
+            </ul>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="<?php echo base_url()?>">Home</a></li>
-                <li><a href="<?php echo base_url("index.php/site/about")?>">About</a></li>
-                <li><a href="<?php echo base_url("index.php/site/contact")?>">Contact</a></li>
-                <li><a href="<?php echo base_url("index.php/site/form")?>">Form</a></li>
-                <li><a href="<?php echo base_url("index.php/site/table")?>">Table</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="../navbar/">My Profile</a></li>
-                <li><a href="./">Feedback</a></li>
-                <li><a href="../navbar-fixed-top/">Logout</a></li>
-            </ul>
-        </div><!--/.nav-collapse -->
+    </header>
+
+    <!-- drawer -->
+    <div class="demo-drawer mdl-layout__drawer mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
+        <header class="demo-drawer-header">
+            <img src="<?php echo base_url("assets/images/user.jpg");?>" class="demo-avatar">
+            <div class="demo-avatar-dropdown">
+                <span>hello@email.com</span>
+                <div class="mdl-layout-spacer"></div>
+                <button id="accbtn" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
+                    <i class="material-icons">arrow_drop_down</i>
+                </button>
+                <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="accbtn">
+                    <button class="mdl-menu__item">hello@email.com</button>
+                    <button class="mdl-menu__item">info@domain.net</button>
+                    <button class="mdl-menu__item">Add another account...</button>
+                </ul>
+            </div>
+        </header>
+        <nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
+            <a class="mdl-navigation__link" href="<?php echo base_url()?>">Home</a>
+            <a class="mdl-navigation__link" href="<?php echo base_url("index.php/site/about")?>">About</a>
+            <a class="mdl-navigation__link" href="<?php echo base_url("index.php/site/contact")?>">Contact</a>
+            <a class="mdl-navigation__link" href="<?php echo base_url("index.php/site/form")?>">Form</a>
+            <a class="mdl-navigation__link" href="<?php echo base_url("index.php/site/table")?>">Table</a>
+            <div class="mdl-layout-spacer"></div>
+            <a class="mdl-navigation__link" href="#"><i class="mdl-color-text--blue-grey-400 material-icons">help_outline</i></a>
+        </nav>
     </div>
-</nav>
 
-<div class="container">
 
+    <!-- content container -->
+    <main class="mdl-layout__content mdl-color--grey-100">
+        <div class="mdl-grid demo-content">
     <!-- Close of Container div in footer -->
